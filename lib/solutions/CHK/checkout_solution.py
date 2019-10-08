e889777eeefbee5f2b05769bd38fd18933738603
sbx_user1051 <sbx_user1051@169.254.148.77>
2019-10-08 15:43:21 +0000

    Tue Oct 08 15:43:21 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -25,37 +25,34 @@ def checkout(skus):
         except KeyError():
             return -1
 
-        if item == "A":
+        if sku == "A":
             items[0] += 1
-        elif item == "B":
+        elif sku == "B":
             items[1] += 1
-        elif item == "C":
+        elif sku == "C":
             items[2] += 1
-        elif item == "D":
+        elif sku == "D":
             items[3] += 1
-    print(items)
+
     items, offers = check_offers(items)
 
     total = sum([prices[item_key[i]] * items[i] for i in range(len(items))]) + offers
 
-    print(int(total))
     return int(total)
 
 
 def check_offers(items):
 
     offers = 0
-    while items[0] > 3:
+    while items[0] >= 3:
         offers += 130
         items[0] -= 3
-    while items[1] > 2:
+
+    while items[1] >= 2:
         offers += 45
         items[1] -= 2
 
     return items, offers
 
-
-if __name__ == "__main__":
-    checkout("AAAABBCCCCD")
 
 
