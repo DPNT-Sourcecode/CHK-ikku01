78d0044023b1659fe5c9e865ec0d2ae2d0874c9b
sbx_user1051 <sbx_user1051@169.254.148.77>
2019-10-08 15:46:21 +0000

    Tue Oct 08 15:46:21 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -16,11 +16,11 @@ def checkout(skus):
     items = [0, 0, 0, 0]
     item_key = ["A", "B", "C", "D"]
 
-    skus = list(skus)
+    skus = list(skus) # Split skus into individual units
 
     for sku in skus:
 
-        try:
+        try:  # Checks each value to make sure it is a valid entry
             item = prices[sku]
         except KeyError():
             return -1
@@ -36,12 +36,22 @@ def checkout(skus):
 
     items, offers = check_offers(items)
 
+    # put each item through pricing scrutiny, then add on the pricing of the special offers
     total = sum([prices[item_key[i]] * items[i] for i in range(len(items))]) + offers
 
     return int(total)
 
 
 def check_offers(items):
+    """Check for valid purchase amounts to trigger Special offer pricing and adjust accordingly.
+
+    Adds the offer pricing into a special category and removes the items from standard pricing scrutiny.
+
+    >>> check_offers([4, 2, 4, 1])
+    175
+    >>> check_offers([2, 0, 5, 6])
+    0
+    """
 
     offers = 0
     while items[0] >= 3:
@@ -53,6 +63,7 @@ def check_offers(items):
         items[1] -= 2
 
     return items, offers
+
 
 
 
